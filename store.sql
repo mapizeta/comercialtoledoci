6db4f49f88a613308e4a0ffe2597451b03716965
Manuel Pizarro <mapiz@debiannote>
2016-09-25 12:45:23 -0300

    Base datos

--- store.sql
+++ store.sql
@@ -1,37 +1,42 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.2.12deb2+deb8u1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Apr 05, 2014 at 05:06 PM
--- Server version: 5.5.25
--- PHP Version: 5.2.17
+-- Servidor: localhost
+-- Tiempo de generación: 25-09-2016 a las 12:38:57
+-- Versión del servidor: 5.5.49-0+deb8u1
+-- Versión de PHP: 5.6.22-0+deb8u1
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- Database: `store`
+-- Base de datos: `store`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `blog`
+-- Estructura de tabla para la tabla `blog`
 --
 
-CREATE TABLE `blog` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `blog` (
+`id` int(11) NOT NULL,
   `title` varchar(150) NOT NULL,
   `blog` text NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
   `user_id` int(11) NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `blog`
+-- Volcado de datos para la tabla `blog`
 --
 
 INSERT INTO `blog` (`id`, `title`, `blog`, `status`, `user_id`, `date_created`) VALUES
@@ -42,18 +47,17 @@ INSERT INTO `blog` (`id`, `title`, `blog`, `status`, `user_id`, `date_created`) 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Estructura de tabla para la tabla `category`
 --
 
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `category` (
+`id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `order` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `order` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `category`
+-- Volcado de datos para la tabla `category`
 --
 
 INSERT INTO `category` (`id`, `name`, `order`) VALUES
@@ -65,19 +69,18 @@ INSERT INTO `category` (`id`, `name`, `order`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `gallery`
+-- Estructura de tabla para la tabla `gallery`
 --
 
-CREATE TABLE `gallery` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `gallery` (
+`id` int(11) NOT NULL,
   `title` varchar(30) NOT NULL,
   `image` varchar(100) NOT NULL,
-  `order` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `order` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `gallery`
+-- Volcado de datos para la tabla `gallery`
 --
 
 INSERT INTO `gallery` (`id`, `title`, `image`, `order`) VALUES
@@ -89,20 +92,19 @@ INSERT INTO `gallery` (`id`, `title`, `image`, `order`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `manufacturer`
+-- Estructura de tabla para la tabla `manufacturer`
 --
 
-CREATE TABLE `manufacturer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `manufacturer` (
+`id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `phone` varchar(15) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `logo` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `logo` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `manufacturer`
+-- Volcado de datos para la tabla `manufacturer`
 --
 
 INSERT INTO `manufacturer` (`id`, `name`, `phone`, `email`, `logo`) VALUES
@@ -112,21 +114,20 @@ INSERT INTO `manufacturer` (`id`, `name`, `phone`, `email`, `logo`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order`
+-- Estructura de tabla para la tabla `order`
 --
 
-CREATE TABLE `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `order` (
+`id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `status` varchar(15) NOT NULL DEFAULT 'pending',
   `payment_confirmed` tinyint(1) NOT NULL DEFAULT '0',
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `date_modified` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `date_modified` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `order`
+-- Volcado de datos para la tabla `order`
 --
 
 INSERT INTO `order` (`id`, `user_id`, `status`, `payment_confirmed`, `date_created`, `date_modified`) VALUES
@@ -138,10 +139,10 @@ INSERT INTO `order` (`id`, `user_id`, `status`, `payment_confirmed`, `date_creat
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_product`
+-- Estructura de tabla para la tabla `order_product`
 --
 
-CREATE TABLE `order_product` (
+CREATE TABLE IF NOT EXISTS `order_product` (
   `order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `price` decimal(6,2) NOT NULL,
@@ -149,7 +150,7 @@ CREATE TABLE `order_product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `order_product`
+-- Volcado de datos para la tabla `order_product`
 --
 
 INSERT INTO `order_product` (`order_id`, `product_id`, `price`, `quantity`) VALUES
@@ -161,22 +162,21 @@ INSERT INTO `order_product` (`order_id`, `product_id`, `price`, `quantity`) VALU
 -- --------------------------------------------------------
 
 --
--- Table structure for table `page`
+-- Estructura de tabla para la tabla `page`
 --
 
-CREATE TABLE `page` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `page` (
+`id` int(11) NOT NULL,
   `page_title` varchar(50) NOT NULL,
   `menu_title` varchar(25) NOT NULL,
   `show_in_menu` tinyint(1) NOT NULL,
   `content` text NOT NULL,
   `css` text NOT NULL,
-  `url` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `url` varchar(25) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `page`
+-- Volcado de datos para la tabla `page`
 --
 
 INSERT INTO `page` (`id`, `page_title`, `menu_title`, `show_in_menu`, `content`, `css`, `url`) VALUES
@@ -187,11 +187,11 @@ INSERT INTO `page` (`id`, `page_title`, `menu_title`, `show_in_menu`, `content`,
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Estructura de tabla para la tabla `product`
 --
 
-CREATE TABLE `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `product` (
+`id` int(11) NOT NULL,
   `sku` varchar(25) NOT NULL,
   `name` varchar(60) NOT NULL,
   `description` text NOT NULL,
@@ -201,53 +201,52 @@ CREATE TABLE `product` (
   `image` varchar(40) DEFAULT NULL,
   `price` decimal(6,2) NOT NULL DEFAULT '0.00',
   `status` tinyint(4) NOT NULL DEFAULT '1',
-  `views` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `views` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `product`
+-- Volcado de datos para la tabla `product`
 --
 
 INSERT INTO `product` (`id`, `sku`, `name`, `description`, `category`, `manufacturer`, `quantity`, `image`, `price`, `status`, `views`) VALUES
 (2, 'ABC123', 'Product 1', 'This is the description', 1, 2, 0, '1.jpg', 4.50, 1, 0),
 (3, 'BCD234', 'Product 2', 'This is the description', 1, 2, 5, '2.jpg', 4.50, 1, 0),
 (4, 'QWE321', 'This is my Product', 'This is the description', 2, 2, 2, '3.jpg', 59.99, 1, 0),
-(5, 'SKU1234', 'My First Form Added Product', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare vulputate enim, sollicitudin interdum turpis imperdiet id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam ac ante magna. Vestibulum vehicula ullamcorper odio condimentum tempor. Donec vulputate odio in nulla auctor iaculis. Ut id enim congue, congue velit vitae, accumsan dolor. In laoreet nec magna eget faucibus. Cras volutpat mi quam, a convallis quam dictum non. Proin vitae dui ut lectus semper hendrerit quis eget elit. Nam felis tellus, hendrerit eget sagittis sit amet, vestibulum quis orci. Sed condimentum, nunc quis varius elementum, nisl urna elementum magna, quis laoreet neque nisi quis risus. In tincidunt urna vel massa elementum laoreet. Sed tempor vehicula nibh, vitae volutpat tellus iaculis id. Proin a orci dolor. Phasellus ut metus eu sem posuere bibendum convallis vel odio.</p>', 5, 1, 3, '1.jpg', 20.00, 1, 0);
+(5, 'SKU1234', 'My First Form Added Product', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare vulputate enim, sollicitudin interdum turpis imperdiet id. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam ac ante magna. Vestibulum vehicula ullamcorper odio condimentum tempor. Donec vulputate odio in nulla auctor iaculis. Ut id enim congue, congue velit vitae, accumsan dolor. In laoreet nec magna eget faucibus. Cras volutpat mi quam, a convallis quam dictum non. Proin vitae dui ut lectus semper hendrerit quis eget elit. Nam felis tellus, hendrerit eget sagittis sit amet, vestibulum quis orci. Sed condimentum, nunc quis varius elementum, nisl urna elementum magna, quis laoreet neque nisi quis risus. In tincidunt urna vel massa elementum laoreet. Sed tempor vehicula nibh, vitae volutpat tellus iaculis id. Proin a orci dolor. Phasellus ut metus eu sem posuere bibendum convallis vel odio.</p>', 5, 1, 3, '1.jpg', 20.00, 1, 0),
+(6, '', '', '', 0, 0, 0, '', 0.00, 0, 0),
+(7, '', '', '', 0, 0, 0, '', 0.00, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product_image`
+-- Estructura de tabla para la tabla `product_image`
 --
 
-CREATE TABLE `product_image` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `product_image` (
+`id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `url` varchar(100) NOT NULL,
   `order` int(11) NOT NULL DEFAULT '0',
-  `description` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product_review`
+-- Estructura de tabla para la tabla `product_review`
 --
 
-CREATE TABLE `product_review` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `product_review` (
+`id` int(11) NOT NULL,
   `name` varchar(15) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `score` int(1) NOT NULL,
   `review` varchar(500) NOT NULL,
-  `productId` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `productId` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `product_review`
+-- Volcado de datos para la tabla `product_review`
 --
 
 INSERT INTO `product_review` (`id`, `name`, `date`, `score`, `review`, `productId`) VALUES
@@ -260,11 +259,11 @@ INSERT INTO `product_review` (`id`, `name`, `date`, `score`, `review`, `productI
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Estructura de tabla para la tabla `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `user` (
+`id` int(11) NOT NULL,
   `title` varchar(5) NOT NULL,
   `forename` varchar(15) NOT NULL,
   `surname` varchar(15) NOT NULL,
@@ -279,15 +278,136 @@ CREATE TABLE `user` (
   `county` varchar(40) NOT NULL,
   `country` varchar(40) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `admin` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Volcado de datos para la tabla `user`
 --
 
 INSERT INTO `user` (`id`, `title`, `forename`, `surname`, `email`, `phone`, `password`, `newsletter`, `address1`, `address2`, `town`, `postcode`, `county`, `country`, `date_created`, `admin`) VALUES
-(1, 'Mr', 'Wilson', 'McCoubrey', 'wilson@mccoubreys.co.uk', '07835441951', '29ef2918ac300330ddbe99af502d85e9dcf3c478d50f9ee06514d8164ea5e026', 0, '44 Magherahamlet Road', 'Address 2', 'Ballynahinch', 'BT24 8PZ', 'Down', 'UK', '2014-02-22 18:31:28', 1),
+(1, 'Mr', 'Wilson', 'McCoubrey', 'mpizarro@amisoft.cl', '07835441951', '974a2be4c0f6db85c78778e367e905f6f4c1b3524505872ade3ddae1d9ef43b8', 0, '44 Magherahamlet Road', 'Address 2', 'Ballynahinch', 'BT24 8PZ', 'Down', 'UK', '2014-02-22 18:31:28', 1),
 (4, 'Mr', 'Mark', 'Wilson', 'mark94wilson@aol.com', '442894439036', 'fa669f95dc83ccd9400fc939a68666720033d5859860f76edcd892e95afb9cc7', 0, '7 The Village', 'Templepatrick', 'Ballyclare', 'BT39 0AA', 'Co Antrim', '', '2014-03-13 19:53:51', 1),
 (5, 'Mr', 'Wilson', 'McCoubrey', 'wilson@w-mccoubrey.co.uk', '07835441951', '29ef2918ac300330ddbe99af502d85e9dcf3c478d50f9ee06514d8164ea5e026', 0, '44 Magherahamlet Road', '', 'Ballynahinch', 'BT248PZ', 'Down', '', '2014-04-05 08:55:23', 0);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `blog`
+--
+ALTER TABLE `blog`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `category`
+--
+ALTER TABLE `category`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `gallery`
+--
+ALTER TABLE `gallery`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `manufacturer`
+--
+ALTER TABLE `manufacturer`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `order`
+--
+ALTER TABLE `order`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `page`
+--
+ALTER TABLE `page`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `product`
+--
+ALTER TABLE `product`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `product_image`
+--
+ALTER TABLE `product_image`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `product_review`
+--
+ALTER TABLE `product_review`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `blog`
+--
+ALTER TABLE `blog`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `category`
+--
+ALTER TABLE `category`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `gallery`
+--
+ALTER TABLE `gallery`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `manufacturer`
+--
+ALTER TABLE `manufacturer`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `order`
+--
+ALTER TABLE `order`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `page`
+--
+ALTER TABLE `page`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `product`
+--
+ALTER TABLE `product`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT de la tabla `product_image`
+--
+ALTER TABLE `product_image`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `product_review`
+--
+ALTER TABLE `product_review`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
